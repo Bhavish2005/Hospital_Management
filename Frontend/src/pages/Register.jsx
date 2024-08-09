505003478677f4a5433c2a0ab57711267bd5b5fc
Bhavish2005 <pushkarna.bhavish2005@gmail.com>
2024-08-09 18:46:27 +0530

    register frontend completed

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,9 +1,142 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { Context } from "../main";
+import { Navigate, useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [nic, setNic] = useState("");
+  const [dob, setDob] = useState("");
+  const [gender, setGender] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Register
+  const navigateTo = useNavigate();
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/api/v1/user/patient/register",
+        { firstName, lastName, email, phone, nic, dob, gender, password },
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      toast.success(response.data.message);
+      setIsAuthenticated(true);
+      navigateTo("/");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+  return (
+    <div className="container form-component register-form">
+      <h1>Sign Up</h1>
+      <p>
+        I Hope You are Going Fine but if it is anything which making you not
+        Feeling Well ,No problem we are here to serve You
+      </p>
+      <p>Sign up To Continue for your Tomorrows Good Health</p>
+      <form onSubmit={handleRegister}>
+        <div>
+          <input
+            type="text"
+            placeholder="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="number"
+            placeholder="Phone Number"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="number"
+            placeholder="NIC"
+            value={nic}
+            onChange={(e) => setNic(e.target.value)}
+          />
+          <input
+            type="date"
+            placeholder="DOB"
+            value={dob}
+            onChange={(e) => setDob(e.target.value)}
+          />
+        </div>
+        <div>
+          <select value={gender} onChange={(e) => setGender(e.target.value)}>
+            <option value="">Select Gender</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div
+          style={{
+            gap: "10px",
+            justifyContent: "space-evenly",
+            flexDirection: "row",
+          }}
+        >
+          <p style={{ marginTop: "10px", marginBottom: "10px" }}>
+            Already have an Account?
+          </p>
+          <Link
+            to={"/login"}
+            style={{
+              textDecoration: "none",
+              color: "grey",
+              alignItems: "center",
+              paddingTop: "10px",
+            }}
+          >
+            Login Now
+          </Link>
+        </div>
+        <div
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "10px",
+          }}
+        >
+          <button type="submit">Register</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
